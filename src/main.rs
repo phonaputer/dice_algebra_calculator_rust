ef01d891eee3dba27bab115eb76ab19ea289ece6
phonaputer <njerome@uwalumni.com>
2026-01-25 20:16:06 +0900

    Add input reading.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Please enter a dice algebra expression: ");
+    if let Err(err) = std::io::stdout().flush() {
+        eprintln!("Error: Failed to flush stdout buffer: {err}");
+        std::process::exit(1);
+    }
+
+    let mut input_buffer = String::new();
+    if let Err(err) = std::io::stdin().read_line(&mut input_buffer) {
+        eprintln!("Error: Failed to read input: {err}");
+        std::process::exit(1);
+    };
+    let input = input_buffer.trim_end();
+
+    println!("You wrote: {input}!");
 }
